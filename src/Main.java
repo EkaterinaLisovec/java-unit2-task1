f7a798d6485bd69146aaf872f8748938b872e26e
EkaterinaLisovec <lisovets_ekaterina@mail.ru>
2020-07-31 17:31:12 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) { //main + Tab
+        int current_account = 2_000_000_000;
+        int transfer = 500_000_000;
+        int total_account = current_account + transfer;
+
+        System.out.println("Total account = " + total_account); //sou + Tab
+    }
+}
+
